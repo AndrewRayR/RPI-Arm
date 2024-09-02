178494d6197743e5ed1e00466bc1fa8b2b34721e
Andrew Richardson <160295237+AndrewRayR@users.noreply.github.com>
2024-09-01 21:25:38 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,7 @@
 import time
 import board
 import busio
-import adafruit_pca9685
 from adafruit_servokit import ServoKit
-i2c = busio.I2C(board.SCL, board.SDA)
-hat = adafruit_pca9685.PCA9685(i2c)
 kit = ServoKit(channels=16)
 
 time.sleep(1)
